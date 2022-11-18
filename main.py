0ac1c7cec247c15f15c77146bf81b11c7d918c13
Camden Davis <camdendavis99@gmail.com>
2022-11-18 01:54:50 -0800

    Add player, sprite, and movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import *
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -15,8 +16,9 @@ screen = pygame.display.set_mode([1080, 720])
 
 def main():
     pygame.init()
-
+    player = Player()
     running = True
+
     while running:
 
         # Check for events
@@ -24,7 +26,15 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
+        # Update player position
+        pressed_keys = pygame.key.get_pressed()
+        if True in pressed_keys:
+            print("key pressed")
+        player.update(pressed_keys)
+
         screen.fill((128, 200, 128))
+        screen.blit(player.surf, player.rect)
+        print(player.rect)
         pygame.display.flip()
 
     pygame.quit()
---
+++ player.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((80, 80, 200))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[pygame.K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[pygame.K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[pygame.K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[pygame.K_RIGHT]:
+            self.rect.move_ip(5, 0)
