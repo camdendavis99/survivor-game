27e3c0ac753f8275b205db56dedc279321d69fce
Camden Davis <camdendavis99@gmail.com>
2022-11-17 17:32:35 -0800

    Add game loop and window

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pygame
 
 
+screen = pygame.display.set_mode([1080, 720])
+
+
 def main():
     pygame.init()
+
+    running = True
+    while running:
+
+        # Check if user closes window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
