ad1160ab12a3d45475e577b10984af0788287ddc
Camden Davis <camdendavis99@gmail.com>
2022-11-18 05:40:49 -0800

    Lower player size and speed

--- player.py
+++ player.py
@@ -4,19 +4,19 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+        self.surf = pygame.Surface((8, 16))
         self.surf.fill((80, 80, 200))
         self.rect = self.surf.get_rect()
 
     def update(self, pressed_keys, screen_size):
         if pressed_keys[pygame.K_UP]:
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -1)
         if pressed_keys[pygame.K_DOWN]:
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, 1)
         if pressed_keys[pygame.K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            self.rect.move_ip(-1, 0)
         if pressed_keys[pygame.K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            self.rect.move_ip(1, 0)
 
         # Keep player on the screen
         if self.rect.left < 0:
